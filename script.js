e98ccac26c8f60a7934470a766209063dd43de1b
weeezik <mkihanya.33@gmail.com>
2024-10-07 00:37:20 -0700

    Edit button listener function

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const userName = document.querySelector('input');
 const joinButton = document.querySelector('button');
+const officialShinobis = []
 
 joinButton.addEventListener("click", function() {
     if(userName) {
-      alert(userName.value);
+      alert(`You, ${userName.value}, are now a New Shinobi.`);
+      officialShinobis.push(userName.value)
+      userName.value = ""
+      console.log(officialShinobis)
     };
   }
 );
